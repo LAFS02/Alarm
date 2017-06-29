bff52e37a86844622936c4efe0dffccd8bc50951
livin <livinmathew99@gmail.com>
2017-06-29 21:21:15 +0530

    Initial Commit

---
+++ app/src/main/java/com/lmntrx/android/alarm/AlarmReceiver.java
@@ -0,0 +1,22 @@
+package com.lmntrx.android.alarm;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Vibrator;
+import android.widget.Toast;
+
+public class AlarmReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        Toast.makeText(context, "Don't panic but your time is up!!!!.",
+                Toast.LENGTH_LONG).show();
+
+        // Vibrate the mobile phone
+        Vibrator vibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
+        long[] pattern = {100,200,300,100,500};
+        vibrator.vibrate(pattern,2);
+    }
+}
---
+++ app/src/main/java/com/lmntrx/android/alarm/AlarmReceiver2.java
@@ -0,0 +1,15 @@
+package com.lmntrx.android.alarm;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class AlarmReceiver2 extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO: This method is called when the BroadcastReceiver is receiving
+        // an Intent broadcast.
+
+    }
+}
